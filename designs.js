c66b16ef81150ecaabcbae3b2de0ed3e76650fd5
mzlovelyj <liljp064@gmail.com>
2018-03-22 01:32:13 -0400

    done

--- designs.js
+++ designs.js
@@ -70,7 +70,7 @@ function paintCell($cell, color) {
         isDragging = false;
     });
 
-    //disabled mouseout
+    //disabled
     document.addEventListener('mouseout', event => event.preventDefault());
 
 })();
